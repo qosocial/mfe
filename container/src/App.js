b09fd06930d8e685ae3540cef2c266fe2a626b2a
[qosocial] <[travisriegler@qosocial.com]>
2023-06-08 10:44:32 -0400

    github actions take 2

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
-//testing the github actions: take 1
+//testing the github actions: take 2
 		
 export default () => {
     
