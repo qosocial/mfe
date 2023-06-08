a7f1fd67c19fa339f32c6dd1920e209f9dbfb8c3
[qosocial] <[travisriegler@qosocial.com]>
2023-06-08 14:49:52 -0400

    testing

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -5,6 +5,8 @@ import { StylesProvider } from '@material-ui/core/styles'
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+//testing github container yaml only
+
 export default () => {
     return <div>
         <StylesProvider>
