a4c9ea79c134660b6fb42970b9fbf3c927f288f2
[qosocial] <[travisriegler@qosocial.com]>
2023-06-12 09:34:41 -0400

    added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
-//testing the github actions: take 7
+import Header from './components/Header'
 		
 export default () => {
     
-    return <div>
-        <h1>hello from container</h1>
-        <MarketingApp />
-    </div>
-    
-    
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    )
 }
